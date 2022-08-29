8a307601268a57fb0b5d65b9429e6a036216aaea
Peter Crawley <peterzcrawley@gmail.com>
2022-08-29 22:38:18 +0100

    Add ProtocolLib as a dependency

--- Server/build.gradle.kts
+++ Server/build.gradle.kts
@@ -8,14 +8,16 @@ repositories {
 
 	maven("https://repo.papermc.io/repository/maven-public/") // Paper
 	maven("https://repo.aikar.co/content/groups/aikar/") // Annotation Command Framework
+	maven("https://repo.dmulloy2.net/repository/public") // ProtocolLib
 	maven("https://jitpack.io") // Vault
 }
 
 dependencies {
 	paperDevBundle("1.19.2-R0.1-SNAPSHOT")
 
-	compileOnly(project(":IonCore"))
+	compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0")
 	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+	compileOnly(project(":IonCore"))
 
 	implementation(project(":Common"))
 
