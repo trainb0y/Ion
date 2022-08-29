1905e81a6b1897fdd8508bb1f4adb196cfadd0df
Peter Crawley <peterzcrawley@gmail.com>
2022-08-29 23:12:43 +0100

    no the other kdoc comments

--- Server/src/main/kotlin/net/horizonsend/ion/server/planets/OrbitingPlanetaryObject.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/planets/OrbitingPlanetaryObject.kt
@@ -1,23 +1,15 @@
 package net.horizonsend.ion.server.planets
 
 interface OrbitingPlanetaryObject: PlanetaryObject {
-	/**
-	 * The X offset from the parent object.
-	 */
+	/** The X offset from the parent object. */
 	val orbitOffsetX: Int
 
-	/**
-	 * The Z offset from the parent object.
-	 */
+	/** The Z offset from the parent object. */
 	val orbitOffsetZ: Int
 
-	/**
-	 * The size of the orbit on the X axis.
-	 */
+	/** The size of the orbit on the X axis. */
 	val orbitSizeX: Int
 
-	/**
-	 * The size of the orbit on the Z axis.
-	 */
+	/** The size of the orbit on the Z axis. */
 	val orbitSizeZ: Int
 }
