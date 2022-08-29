68ed33c37c6cf0a5de0bee7836e4cd98eaadb2d7
Peter Crawley <peterzcrawley@gmail.com>
2022-08-29 22:59:26 +0100

    Basic OrbitingPlanetaryObject Boilerplate

--- Server/src/main/kotlin/net/horizonsend/ion/server/planets/OrbitingPlanetaryObject.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/planets/OrbitingPlanetaryObject.kt
@@ -1,3 +1,23 @@
 package net.horizonsend.ion.server.planets
 
-interface OrbitingPlanetaryObject
+interface OrbitingPlanetaryObject: PlanetaryObject {
+	/**
+	 * The X offset from the parent object.
+	 */
+	val orbitOffsetX: Int
+
+	/**
+	 * The Z offset from the parent object.
+	 */
+	val orbitOffsetZ: Int
+
+	/**
+	 * The size of the orbit on the X axis.
+	 */
+	val orbitSizeX: Int
+
+	/**
+	 * The size of the orbit on the Z axis.
+	 */
+	val orbitSizeZ: Int
+}
