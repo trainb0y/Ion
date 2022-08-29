790453942408bd4bb8cb042afd6d005a458bd47f
Peter Crawley <peterzcrawley@gmail.com>
2022-08-29 23:22:22 +0100

    RougePlanet

---
+++ Server/src/main/kotlin/net/horizonsend/ion/server/planets/RougePlanet.kt
@@ -0,0 +1,18 @@
+package net.horizonsend.ion.server.planets
+
+import net.kyori.adventure.text.Component
+
+/** @see StationaryPlanetaryObject */
+data class RougePlanet(
+	override val name: String,
+	override val x: Int,
+	override val z: Int,
+	override val orbitingObjects: Array<OrbitingPlanetaryObject> = arrayOf()
+) : StationaryPlanetaryObject {
+	override val displayName: Component = Component.text(name.replaceFirstChar { it.uppercase() })
+
+	// We only bother implementing this to avoid a editor warning
+	// The code only identifies them by name so that alone is enough
+	override fun equals(other: Any?): Boolean = (other is Star) && name == other.name
+	override fun hashCode(): Int = name.hashCode()
+}
