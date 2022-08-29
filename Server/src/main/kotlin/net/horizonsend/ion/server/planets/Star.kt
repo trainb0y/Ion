258ebd86348dd3eea5b347c7401f2182461c3138
Peter Crawley <peterzcrawley@gmail.com>
2022-08-29 23:11:25 +0100

    kdoc comments

--- Server/src/main/kotlin/net/horizonsend/ion/server/planets/Star.kt
+++ Server/src/main/kotlin/net/horizonsend/ion/server/planets/Star.kt
@@ -4,9 +4,11 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextColor
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 
+/** Represents a big flaming ball of gas in space, specified by configuration. */
 @ConfigSerializable
 data class Star(
 	override val name: String,
+	/** Used to colour the displayName of the star. */
 	val starColour: String,
 	override val x: Int,
 	override val z: Int,
